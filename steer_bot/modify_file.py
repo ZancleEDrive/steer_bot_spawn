d215d1b4aaae0e3f0d1f8f3471029446e2de357a
Luca D'Agati <66645997+lucadagati@users.noreply.github.com>
2023-09-27 22:35:24 +0200

    Update modify_file.py

--- steer_bot/modify_file.py
+++ steer_bot/modify_file.py
@@ -1,10 +1,10 @@
 import sys
 
-def modify_file(file_path):
+def modify_python_file(file_path):
     try:
         with open(file_path, 'r') as file:
             file_data = file.read()
-        
+
         new_data = file_data.replace("math.sin(ix / 10.0) * ix / 5.0", "math.sin(ix / 8.33) * ix / 4.0")
         
         with open(file_path, 'w') as file:
@@ -13,12 +13,39 @@ def modify_file(file_path):
         print(f"Modifica di {file_path} completata.")
     
     except Exception as e:
-        print(f"Errore: {e}")
+        print(f"Errore nella modifica del file Python: {e}")
+
+def modify_launch_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            file_data = file.readlines()
+
+        # Verifica se la modifica è già stata applicata per evitare duplicati
+        if any("worlds/new_world.world" in line for line in file_data):
+            print(f"Modifica già applicata a {file_path}.")
+            return
+
+        for i, line in enumerate(file_data):
+            if '<arg name="world_name" default="/worlds/empty_world"/>' in line:
+                file_data[i] = f"<!-- {line.strip()} -->\n"
+                file_data.insert(i + 1, '  <arg name="world_name" default="/worlds/new_world.world"/>\n')
+                break
+
+        with open(file_path, 'w') as file:
+            file.writelines(file_data)
+
+        print(f"Modifica di {file_path} completata.")
+    
+    except Exception as e:
+        print(f"Errore nella modifica del file launch: {e}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Uso: python modify_file.py <percorso-del-file>")
+    if len(sys.argv) != 3:
+        print("Uso: python modify_files.py <percorso-del-file-python> <percorso-del-file-launch>")
         sys.exit(1)
-    
-    file_path = sys.argv[1]
-    modify_file(file_path)
+
+    python_file_path = sys.argv[1]
+    launch_file_path = sys.argv[2]
+
+    modify_python_file(python_file_path)
+    modify_launch_file(launch_file_path)

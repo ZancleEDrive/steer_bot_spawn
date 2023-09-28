3fe0223b8027aeeadb4c0bcc4a31695d6a7a0182
Luca D'Agati <66645997+lucadagati@users.noreply.github.com>
2023-09-28 12:07:13 +0200

    Update modify_file.py

--- steer_bot/modify_file.py
+++ steer_bot/modify_file.py
@@ -5,15 +5,23 @@ def modify_python_file(file_path):
         with open(file_path, 'r') as file:
             file_data = file.read()
         
-        if "math.sin(ix / 8.33) * ix / 4.0" not in file_data:  # Verifica se la modifica è già stata effettuata
-            new_data = file_data.replace("math.sin(ix / 10.0) * ix / 5.0", "math.sin(ix / 8.33) * ix / 4.0")
-            
+        # Verifica se le modifiche sono già state effettuate
+        modified = False
+        if "math.sin(ix / 8.33) * ix / 4.0" not in file_data:
+            file_data = file_data.replace("math.sin(ix / 10.0) * ix / 5.0", "math.sin(ix / 8.33) * ix / 4.0")
+            modified = True
+
+        if "cx = np.arange(0, 60, 0.5)" not in file_data:
+            file_data = file_data.replace("cx = np.arange(0, 50, 0.5)", "cx = np.arange(0, 60, 0.5)")
+            modified = True
+        
+        if modified:
             with open(file_path, 'w') as file:
-                file.write(new_data)
+                file.write(file_data)
 
             print(f"Modifica di {file_path} completata.")
         else:
-            print(f"Modifica già applicata a {file_path}.")
+            print(f"Modifiche già applicate a {file_path}.")
     
     except Exception as e:
         print(f"Errore nella modifica del file Python: {e}")
